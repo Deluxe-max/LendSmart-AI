b10f05a12b7b9be520fdc5f4441139d6b860b229
Deluxe-max <joannaade123@gmail.com>
2026-05-05 08:38:25 +0100

    Updated template path to include predictor folder

--- settings.py
+++ settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'predictor' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
